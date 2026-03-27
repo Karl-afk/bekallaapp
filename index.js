bb7f0b335d21175eb02fcc7866303b131deecdf2
Karl Weinmüller <karlweinmueller@me.com>
2026-03-27 06:40:22 +0100

    test env variables

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const myVariable = process.env.MYVARIABLE || 'default';
 
 app.get('/', (req, res) => {
-  res.send('Hello World! from E');
+  res.send('Hello World! from E, myVar: ' + myVariable);
 });
 
 app.listen(port, () => {
